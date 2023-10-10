225c1ed1860e433e5857222c72565931e420ba11
Yassine Oujama <122239886+Y4xx@users.noreply.github.com>
2023-10-10 14:16:54 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,27 +1,27 @@
 #include "main.h"
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
+ * jack_bauer - Y4x
  * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
-	int x, y;
-	
-	x = 0;
+	int i, j;
 
-	while (x < 24)
+	i = 0;
+
+	while (i < 24)
 	{
-		y = 0;
-		while (y < 60)
+		j = 0;
+		while (j < 60)
 		{
-			_putchar((x / 10) + '0');
-			_putchar((x % 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((y / 10) + '0');
-			_putchar((y % 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
-			y++;
+			j++;
 		}
-		x++;
+		i++;
 	}
 }

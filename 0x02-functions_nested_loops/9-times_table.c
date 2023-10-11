ced91b8b33263e056a9128c1ce4660967253514f
Yassine Oujama <122239886+Y4xx@users.noreply.github.com>
2023-10-11 01:00:14 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,36 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table - Y4x.
- *
- * Description: This function prints the multiplication table for the number 9,
- * starting from 0, up to 9 times 9.
+ * times_table - prints the 9 times table, starting with 0
  */
 void times_table(void)
 {
-	int row, column, product;
+	int i, j, k;
 
-	for (row = 0; row <= 9; row++)
+	for (i = 0; i < 10; i++)
 	{
-		for (column = 0; column <= 9; column++)
+		for (j = 0; j < 10; j++)
 		{
-			product = row * column;
-
-			if (column == 0)
+			k = j * i;
+			if (j == 0)
 			{
-				_putchar('0');
-			}
-			else if (product < 10)
-			{
-				_putchar(' ');	
-				_putchar(product + '0');
-			}
-			else
-			{
-				_putchar(' ');
-				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
+				_putchar(k + '0');
 			}
 
-			if (column != 9)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
-
 		_putchar('\n');
 	}
 }

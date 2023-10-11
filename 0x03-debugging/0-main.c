adab125802dca9f53ec678243c51a69e43773626
Yassine Oujama <122239886+Y4xx@users.noreply.github.com>
2023-10-11 13:40:08 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
-* main - Test function for positive or negative
-* Return: 0
+* main - prints if integer is positive or negative
+* Return: 0 (code success)
 */
+
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }

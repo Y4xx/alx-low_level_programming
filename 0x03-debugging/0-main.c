bf1e460abc819fca28d5fb03ddea09b6043a0c0c
Yassine Oujama <122239886+Y4xx@users.noreply.github.com>
2023-10-11 13:12:03 +0100

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,12 @@
 #include "main.h"
 /**
-* main - Entry point - Y4x
-* 
-* Return: Always 0 (Success)
+* main - Test function for positive or negative
+* Return: 0
 */
 int main(void)
 {
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }

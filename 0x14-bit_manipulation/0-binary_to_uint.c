f190bd5efb22ea681f8b20f794a45a52ce1ee425
Yassine Oujama <yassine.oujama@gmail.com>
2023-11-24 17:43:28 -0800

    fix 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,27 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary to an unsigned int
- * @b: the binary number as a string
- *
- * Return: the converted value
+ * binary_to_uint - convert the binary number to a decimal
+ * @b: points to a string of 0's and 1's
+ * Return: decimal format of the binary
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int decimal = 0;
-int str_len = 0, base = 1;
+	unsigned int dec = 0; /* dec is the variable to hold the decimal format */
 
-if (!check_valid_string(b))
-return (0);
+	if (b == NULL)
+		return (0);
 
-while (b[str_len] != '\0')
-str_len++;
+	while (*b)
+	{
+		if (*b == '1')
+			dec = (dec << 1) | 1;
+		else if (*b == '0')
+			dec <<= 1;
+		else
+			return (0);
+		b++;
+	}
 
-while (str_len)
-{
-decimal += ((b[str_len - 1] - '0') * base);
-base *= 2;
-str_len--;
-}
-return (decimal);
-}
-
-/**
- * check_valid_string - checks if a string has only 0's and 1's
- * @b: string to be checked
- *
- * Return: 1 if string is valid, 0 otherwise
- */
-int check_valid_string(const char *b)
-{
-if (b == NULL)
-return (0);
-
-while (*b)
-{
-if (*b != '1' && *b != '0')
-return (0);
-b++;
-}
-return (1);
+	return (dec);
 }

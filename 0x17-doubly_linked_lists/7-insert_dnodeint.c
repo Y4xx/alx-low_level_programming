38b11ada722288132e29032963d53fad3cb2bece
root <root@Y4x.localdomain>
2024-01-12 00:22:44 +0100

    fix 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,14 +1,51 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: pointer to head of list
- * @idx: index of the list where the new node should be added
- * @n: data of the new node
- * 
- * Return:  the address of the new node, or NULL if it failed
+ * insert_dnodeint_at_index - check the code
+ * @h: input
+ * @idx: input
+ * @n: input
+ * Return: check code
  */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *temp, *node;
+	unsigned int i = 1;
+
+	temp = *h;
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = n;
+	node->prev = NULL;
+	node->next = NULL;
+	if ((*h) == NULL)
+	{
+		if (idx == 0)
+		{
+			*h = node;
+			return (node);
+		}
+		return (NULL);
+	}
+	if (idx == 0)
+	{
+		node->next = *h;
+		(*h)->prev = node;
+		*h = node;
+		return (node);
+	}
+	for (; temp->next != NULL && i != idx; i++)
+		temp = temp->next;
+	if (i == idx)
+	{
+		node->prev = temp;
+		node->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = node;
+		temp->next = node;
+		return (node);
+	}
+	return (NULL);
 }
